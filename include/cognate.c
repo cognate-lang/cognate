84f1a1bf147acf4b7090de7b30429dbe3f077447
Finn Barber <finnbarber@protonmail.com>
2020-12-04 11:23:40 +0000

    Rephrased error message

--- include/cognate.c
+++ include/cognate.c
@@ -92,7 +92,7 @@ static void check_call_stack()
     // if (how much stack left < stack change between checks)
     if (unlikely(stack_max.rlim_cur + &b - stack_start < stack_start - &b - old_stack_size))
     {
-      throw_error_fmt("Call stack overflow! Too much recursion! (call stack is %ti bytes)", stack_start - &b);
+      throw_error_fmt("Call stack overflow - too much recursion! (call stack is %ti bytes)", stack_start - &b);
     }
     old_stack_size = stack_start - &b;
   }
