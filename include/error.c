a1ac03d7efc8098d4e63729bf6ec8d744c23b482
Finn Barber <finnbarber@protonmail.com>
2020-12-26 15:23:30 +0000

    Simplified throw_error(), as well as making it actually print to stderr
    Simplified throw_error(), as well as making it actually print to stderr

--- include/error.c
+++ include/error.c
@@ -19,27 +19,26 @@ __attribute__((noreturn)) static void throw_error(const char* const fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  // Here we use vsnprintf to calculate out buffer size before actually filling it with vsprintf.
-  char* const message = (char*) cognate_malloc (vsnprintf(NULL, 0, fmt, args) + 1); 
-  vsprintf(message, fmt, args);
-  va_end(args);
   struct winsize term;
   // If we cannot determine the terminal size (redirected to file or something), assume width is 80.
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &term) == -1) term.ws_col = 80;
   long i;
   // Print the title bar.
-  for (i = 0; i < (term.ws_col - 18) / 2; ++i) fputs("\342\224\200", stdout);
-  fputs("\342\224\244 \033[0;1mCognate Error!\033[0m \342\224\234", stdout);
-  for (i = 0; i < (term.ws_col - 17) / 2; ++i) fputs("\342\224\200", stdout);
+  for (i = 0; i < (term.ws_col - 18) / 2; ++i) fputs("\342\224\200", stderr);
+  fputs("\342\224\244 \033[0;1mCognate Error!\033[0m \342\224\234", stderr);
+  for (i = 0; i < (term.ws_col - 17) / 2; ++i) fputs("\342\224\200", stderr);
   // Print generic error header.
-  puts("\nCognate has encountered an unrecoverable error.\n"
-         "Details are below:\n");
+  fputs("\nCognate has encountered an unrecoverable error.\n"
+         "Details are below:\n", stderr);
   // Print the function name, if inside a function.
-  if (function_name != NULL) printf("\033[0;1mIn function '%c%s'...\033[0m\n\n", toupper(*function_name), function_name+1);
+  if (function_name != NULL) fprintf(stderr, "\033[0;1mIn function '%c%s'...\033[0m\n\n", toupper(*function_name), function_name+1);
   // Actually print the error message now.
-  printf("\033[31;1m%s\033[0m\n", message);
+  fprintf(stderr, "\033[31;1m");
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+  fprintf(stderr, "\033[0m\n");
   // Print the bottom row thing.
-  for (i = 0; i < term.ws_col; ++i) fputs("\342\224\200", stdout);
+  for (i = 0; i < term.ws_col; ++i) fputs("\342\224\200", stderr);
   // Exit, with error.
   exit(-1);
 }
