563f51bb9a1535328511f0c853faec109abfd81a
Finn Barber <finnbarber@protonmail.com>
2021-01-15 14:52:11 +0000

    Use cognate_type consistently for types, instead of casting to char.

--- include/type.c
+++ include/type.c
@@ -4,7 +4,7 @@
 #include "cognate.h"
 #include "types.h"
 
-static cognate_object check_type(char, cognate_object);
+static cognate_object check_type(cognate_type, cognate_object);
 static const char* lookup_type(cognate_type);
 static _Bool compare_objects(cognate_object, cognate_object);
 static _Bool compare_lists(cognate_list, cognate_list);
@@ -14,7 +14,7 @@ static _Bool compare_tables(cognate_table, cognate_table);
 #include "table.c"
 #include <string.h>
 
-static cognate_object check_type(char expected_type, cognate_object object)
+static cognate_object check_type(cognate_type expected_type, cognate_object object)
 {
   if likely(object.type & expected_type)
     return object;
