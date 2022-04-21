b15a3bb445cba2ad31eefa39329ee4a4897c85cd
Finn Barber <finnbarber@protonmail.com>
2022-04-21 16:35:49 +0100

    remove flto again

--- src/cognac.c
+++ src/cognac.c
@@ -546,7 +546,8 @@ int main(int argc, char** argv)
 	{
 		"clang", c_file_path, "-o", binary_file_path, "-fblocks", "-I"INCLUDEDIR, "-lBlocksRuntime",
 		"-lpthread", release ? "-Ofast" : "-O1", "-Wall", "-Wextra", "-Werror", "-Wno-unused", "-pedantic-errors",
-		"-std=c11", "-lm", "-g0", release ? "-s" : "-ggdb3", "-fuse-ld=lld", NULL
+		"-std=c11", "-lm", "-g0", release ? "-s" : "-ggdb3", NULL
+		// "-flto" and "-fuse-ld=lld" give smaller binaries but make installation a pain
 	};
 	fflush(outfile);
 	if (fork() == 0) execvp(args[0], args); else wait(NULL);
